3a2f98ce2ae304c1d6e3fba4d0b625156e775a51
Shaojung <abc@abc.com>
2016-09-21 19:37:02 +0800

    加入 Click

--- app/src/main/java/tw/com/pcschool/t092101/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t092101/MainActivity.java
@@ -2,8 +2,11 @@ package tw.com.pcschool.t092101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
                                         android.R.layout.simple_list_item_1,
                                         fruits);
         lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Click: " + fruits[position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
