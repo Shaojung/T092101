d8ccc59faa1872d207ea48a4cb0e509ac5e5f498
Shaojung <abc@abc.com>
2016-09-21 19:10:14 +0800

    ArrayAdapter 完成

--- app/src/main/java/tw/com/pcschool/t092101/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t092101/MainActivity.java
@@ -2,12 +2,25 @@ package tw.com.pcschool.t092101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] fruits = {"蘋果","橘子", "香蕉", "鳳梨", "柚子"};
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                        MainActivity.this,
+                                        android.R.layout.simple_list_item_1,
+                                        fruits);
+        lv.setAdapter(adapter);
+
     }
 }
